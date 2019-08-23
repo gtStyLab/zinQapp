6cf1a58958ce56295ea4388be488f7d9aa82ebd1
Krishi Kishore <krishikishore10@gmail.com>
2019-08-23 19:19:11 -0400

    drawable added

--- app/src/main/java/com/krishikishore/zin_q/ResultsActivity.java
+++ app/src/main/java/com/krishikishore/zin_q/ResultsActivity.java
@@ -79,7 +79,7 @@ public class ResultsActivity extends AppCompatActivity {
         String radius6 = intentExtras.getStringExtra("Radius6");
 
         //line fitting
-        double xcoordinates[] = { 0.5, 1, 2, 5, 10 };
+        double xcoordinates[] = { Math.log10(0.5), Math.log10(1), Math.log10(2), Math.log10(5), Math.log10(10) };
         double redplot[] = {Double.parseDouble(red1), Double.parseDouble(red2), Double.parseDouble(red3),Double.parseDouble(red4), Double.parseDouble(red5)} ;
         double greenplot[] = {Double.parseDouble(green1), Double.parseDouble(green2), Double.parseDouble(green3),Double.parseDouble(green4), Double.parseDouble(green5)};
         double blueplot[] = {Double.parseDouble(blue1), Double.parseDouble(blue2), Double.parseDouble(blue3),Double.parseDouble(blue4), Double.parseDouble(blue5)};
@@ -88,10 +88,10 @@ public class ResultsActivity extends AppCompatActivity {
         float m, c, sum_x = 0, sum_y = 0, sum_xy = 0, sum_x2 = 0;
         int n = xcoordinates.length;
         for (i = 0; i < n; i++) {
-            sum_x += xcoordinates[i];
-            sum_y += redplot[i];
+            sum_x += redplot[i];
+            sum_y += xcoordinates[i];
             sum_xy += xcoordinates[i] * redplot[i];
-            sum_x2 += (xcoordinates[i] * xcoordinates[i]);
+            sum_x2 += (redplot[i] * redplot[i]);
         }
 
         float redslope = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - (sum_x * sum_x));
@@ -101,10 +101,10 @@ public class ResultsActivity extends AppCompatActivity {
         float m2, c2, sum_x4 = 0, sum_y2 = 0, sum_xy2 = 0, sum_x22 = 0;
         int n2 = xcoordinates.length;
         for (i2 = 0; i2 < n2; i2++) {
-            sum_x4 += xcoordinates[i2];
-            sum_y2 += greenplot[i2];
+            sum_x4 += greenplot[i2];
+            sum_y2 += xcoordinates[i2];
             sum_xy2 += xcoordinates[i2] * greenplot[i2];
-            sum_x22 += (xcoordinates[i2] * xcoordinates[i2]);
+            sum_x22 += (greenplot[i2] * greenplot[i2]);
         }
 
         float greenslope = (n2 * sum_xy2 - sum_x4 * sum_y2) / (n2 * sum_x22 - (sum_x4 * sum_x4));
@@ -114,10 +114,10 @@ public class ResultsActivity extends AppCompatActivity {
         float m3, c3, sum_x6 = 0, sum_y3 = 0, sum_xy3 = 0, sum_x23 = 0;
         int n3 = xcoordinates.length;
         for (i3 = 0; i3 < n3; i3++) {
-            sum_x6 += xcoordinates[i3];
-            sum_y3 += blueplot[i3];
+            sum_x6 += blueplot[i3];
+            sum_y3 += xcoordinates[i3];
             sum_xy3 += xcoordinates[i3] * blueplot[i3];
-            sum_x23 += (xcoordinates[i3] * xcoordinates[i3]);
+            sum_x23 += (blueplot[i3] * blueplot[i3]);
         }
 
         float blueslope = (n3 * sum_xy3 - sum_x6 * sum_y3) / (n3 * sum_x23 - (sum_x6 * sum_x6));
@@ -127,21 +127,17 @@ public class ResultsActivity extends AppCompatActivity {
         double greenvalue = Double.parseDouble(green6);
         double bluevalue = Double.parseDouble(blue6);
 
-        double redparvalue = redvalue-redslope;
-        double greenparvalue = greenvalue-greenslope;
-        double blueparvalue = bluevalue-blueslope;
+        double intervalred = ((redvalue)*(redslope)) + redint;
+        double intervalgreen = ((greenvalue)*(greenslope)) + greenint;
+        double intervalblue = ((bluevalue)*(blueslope)) + blueint;
 
-        double finalredval = redparvalue/redint;
-        double finalgreenval = greenparvalue/greenint;
-        double finalblueval = blueparvalue/blueint;
+        double coloraverage = (intervalred+intervalgreen+intervalblue)/3;
 
-        double finalsum = finalredval+finalgreenval+finalblueval;
-
-        double experimentalcolorscore =  finalsum/3;
+        double finalconcentration = Math.pow(10, coloraverage);
+        finalconcentration = finalconcentration*4;
 
 
-
-        View colorRectangle6 = (View)findViewById(R.id.finalcolor6);
+        View colorRectangle6 = (View)findViewById(R.id.color6);
         Drawable background6 = colorRectangle6.getBackground();
         if (background6 instanceof ShapeDrawable) {
             ((ShapeDrawable)background6).getPaint().setColor(Color.rgb(Integer.parseInt(red6), Integer.parseInt(green6), Integer.parseInt(blue6)));
@@ -151,31 +147,25 @@ public class ResultsActivity extends AppCompatActivity {
             ((ColorDrawable)background6).setColor(Color.rgb(Integer.parseInt(red6), Integer.parseInt(green6), Integer.parseInt(blue6)));
         }
 
-        double doubleColorScore6 = experimentalcolorscore;
+        double doubleColorScore6 = finalconcentration;
         String colorScore6a = String.format("%.3f", doubleColorScore6);
-        TextView colorScoreDisplay6 = (TextView) findViewById(R.id.colorScore6);
-        colorScoreDisplay6.setText(colorScore6a);
+        TextView colorScoreDisplay6 = (TextView) findViewById(R.id.concentration);
+        colorScoreDisplay6.setText("Concentration: " + colorScore6a);
 
-        TextView coordinatesDisplay6 = (TextView) findViewById(R.id.coordinates6);
-        coordinatesDisplay6.setText("(X, Y): (" + x6 +", " + y6 +")");
+        TextView redDisplay6 = (TextView) findViewById(R.id.red6);
+        redDisplay6.setText(red6);
 
-        TextView radiusDisplay6 = (TextView) findViewById(R.id.radius6);
-        radiusDisplay6.setText("Radius: " + radius6 + " pixels");
+        TextView greenDisplay6 = (TextView) findViewById(R.id.green6);
+        greenDisplay6.setText(green6);
 
-        TextView redDisplay6 = (TextView) findViewById(R.id.redvalue6);
-        redDisplay6.setText("Red: " + red6);
+        TextView blueDisplay6 = (TextView) findViewById(R.id.blue6);
+        blueDisplay6.setText(blue6);
 
-        TextView greenDisplay6 = (TextView) findViewById(R.id.greenvalue6);
-        greenDisplay6.setText("Green: " + green6);
+        TextView levelDisplay6 = (TextView) findViewById(R.id.level);
 
-        TextView blueDisplay6 = (TextView) findViewById(R.id.bluevalue6);
-        blueDisplay6.setText("Blue: " + blue6);
-
-        TextView levelDisplay6 = (TextView) findViewById(R.id.level6);
-
-        if (doubleColorScore6 < 1.5) {
+        if (doubleColorScore6 < 8) {
             levelDisplay6.setText("Level: Low");
-        } else if (doubleColorScore6 < 3) {
+        } else if (doubleColorScore6 < 15) {
             levelDisplay6.setText("Level: Borderline");
         } else {
             levelDisplay6.setText("Level: High");
@@ -185,7 +175,7 @@ public class ResultsActivity extends AppCompatActivity {
 
 
 
-        View colorRectangle1 = (View)findViewById(R.id.finalcolor);
+        View colorRectangle1 = (View)findViewById(R.id.color1);
         Drawable background1 = colorRectangle1.getBackground();
         if (background1 instanceof ShapeDrawable) {
             ((ShapeDrawable)background1).getPaint().setColor(Color.rgb(Integer.parseInt(red1), Integer.parseInt(green1), Integer.parseInt(blue1)));
@@ -196,34 +186,20 @@ public class ResultsActivity extends AppCompatActivity {
         }
 
 
-        TextView colorScoreDisplay1 = (TextView) findViewById(R.id.colorScore);
-        colorScoreDisplay1.setText(colorScore1);
+        TextView redDisplay1 = (TextView) findViewById(R.id.red1);
+        redDisplay1.setText(red1);
 
-        TextView coordinatesDisplay1 = (TextView) findViewById(R.id.coordinates);
-        coordinatesDisplay1.setText("(X, Y): (" + x1 +", " + y1 +")");
+        TextView greenDisplay1 = (TextView) findViewById(R.id.green1);
+        greenDisplay1.setText(green1);
 
-        TextView radiusDisplay1 = (TextView) findViewById(R.id.radius);
-        radiusDisplay1.setText("Radius: " + radius1 + " pixels");
-
-        TextView redDisplay1 = (TextView) findViewById(R.id.redvalue);
-        redDisplay1.setText("Red: " + red1);
-
-        TextView greenDisplay1 = (TextView) findViewById(R.id.greenvalue);
-        greenDisplay1.setText("Green: " + green1);
-
-        TextView blueDisplay1 = (TextView) findViewById(R.id.bluevalue);
-        blueDisplay1.setText("Blue: " + blue1);
-
-        TextView levelDisplay1 = (TextView) findViewById(R.id.level);
-        levelDisplay1.setText("Level: Borderline");
+        TextView blueDisplay1 = (TextView) findViewById(R.id.blue1);
+        blueDisplay1.setText(blue1);
 
 
 
 
 
-
-
-        View colorRectangle2 = (View)findViewById(R.id.finalcolor2);
+        View colorRectangle2 = (View)findViewById(R.id.color2);
         Drawable background2 = colorRectangle2.getBackground();
         if (background2 instanceof ShapeDrawable) {
             ((ShapeDrawable)background2).getPaint().setColor(Color.rgb(Integer.parseInt(red2), Integer.parseInt(green2), Integer.parseInt(blue2)));
@@ -234,33 +210,20 @@ public class ResultsActivity extends AppCompatActivity {
         }
 
 
-        TextView colorScoreDisplay2 = (TextView) findViewById(R.id.colorScore2);
-        colorScoreDisplay2.setText(colorScore2);
+        TextView redDisplay2 = (TextView) findViewById(R.id.red2);
+        redDisplay2.setText(red2);
 
-        TextView coordinatesDisplay2 = (TextView) findViewById(R.id.coordinates2);
-        coordinatesDisplay2.setText("(X, Y): (" + x2 +", " + y2 +")");
+        TextView greenDisplay2 = (TextView) findViewById(R.id.green2);
+        greenDisplay2.setText(green2);
 
-        TextView radiusDisplay2 = (TextView) findViewById(R.id.radius2);
-        radiusDisplay2.setText("Radius: " + radius2 + " pixels");
-
-        TextView redDisplay2 = (TextView) findViewById(R.id.redvalue2);
-        redDisplay2.setText("Red: " + red2);
-
-        TextView greenDisplay2 = (TextView) findViewById(R.id.greenvalue2);
-        greenDisplay2.setText("Green: " + green2);
-
-        TextView blueDisplay2 = (TextView) findViewById(R.id.bluevalue2);
-        blueDisplay2.setText("Blue: " + blue2);
-
-        TextView levelDisplay2 = (TextView) findViewById(R.id.level2);
-        levelDisplay2.setText("Level: Borderline");
+        TextView blueDisplay2 = (TextView) findViewById(R.id.blue2);
+        blueDisplay2.setText(blue2);
 
 
 
 
 
-
-        View colorRectangle3 = (View)findViewById(R.id.finalcolor3);
+        View colorRectangle3 = (View)findViewById(R.id.color3);
         Drawable background3 = colorRectangle3.getBackground();
         if (background3 instanceof ShapeDrawable) {
             ((ShapeDrawable)background3).getPaint().setColor(Color.rgb(Integer.parseInt(red3), Integer.parseInt(green3), Integer.parseInt(blue3)));
@@ -270,33 +233,20 @@ public class ResultsActivity extends AppCompatActivity {
             ((ColorDrawable)background3).setColor(Color.rgb(Integer.parseInt(red3), Integer.parseInt(green3), Integer.parseInt(blue3)));
         }
 
+        TextView redDisplay3 = (TextView) findViewById(R.id.red3);
+        redDisplay3.setText(red3);
 
-        TextView colorScoreDisplay3 = (TextView) findViewById(R.id.colorScore3);
-        colorScoreDisplay3.setText(colorScore3);
+        TextView greenDisplay3 = (TextView) findViewById(R.id.green3);
+        greenDisplay3.setText(green3);
 
-        TextView coordinatesDisplay3 = (TextView) findViewById(R.id.coordinates3);
-        coordinatesDisplay3.setText("(X, Y): (" + x3 +", " + y3 +")");
-
-        TextView radiusDisplay3 = (TextView) findViewById(R.id.radius3);
-        radiusDisplay3.setText("Radius: " + radius3 + " pixels");
-
-        TextView redDisplay3 = (TextView) findViewById(R.id.redvalue3);
-        redDisplay3.setText("Red: " + red3);
-
-        TextView greenDisplay3 = (TextView) findViewById(R.id.greenvalue3);
-        greenDisplay3.setText("Green: " + green3);
-
-        TextView blueDisplay3 = (TextView) findViewById(R.id.bluevalue3);
-        blueDisplay3.setText("Blue: " + blue3);
-
-        TextView levelDisplay3 = (TextView) findViewById(R.id.level3);
-        levelDisplay3.setText("Level: Borderline");
+        TextView blueDisplay3 = (TextView) findViewById(R.id.blue3);
+        blueDisplay3.setText(blue3);
 
 
 
 
 
-        View colorRectangle4 = (View)findViewById(R.id.finalcolor4);
+        View colorRectangle4 = (View)findViewById(R.id.color4);
         Drawable background4 = colorRectangle4.getBackground();
         if (background4 instanceof ShapeDrawable) {
             ((ShapeDrawable)background4).getPaint().setColor(Color.rgb(Integer.parseInt(red4), Integer.parseInt(green4), Integer.parseInt(blue4)));
@@ -307,31 +257,19 @@ public class ResultsActivity extends AppCompatActivity {
         }
 
 
-        TextView colorScoreDisplay4 = (TextView) findViewById(R.id.colorScore4);
-        colorScoreDisplay4.setText(colorScore4);
+        TextView redDisplay4 = (TextView) findViewById(R.id.red4);
+        redDisplay4.setText(red4);
 
-        TextView coordinatesDisplay4 = (TextView) findViewById(R.id.coordinates4);
-        coordinatesDisplay4.setText("(X, Y): (" + x4 +", " + y4 +")");
+        TextView greenDisplay4 = (TextView) findViewById(R.id.green4);
+        greenDisplay4.setText(green4);
 
-        TextView radiusDisplay4 = (TextView) findViewById(R.id.radius4);
-        radiusDisplay4.setText("Radius: " + radius4 + " pixels");
-
-        TextView redDisplay4 = (TextView) findViewById(R.id.redvalue4);
-        redDisplay4.setText("Red: " + red4);
-
-        TextView greenDisplay4 = (TextView) findViewById(R.id.greenvalue4);
-        greenDisplay4.setText("Green: " + green4);
-
-        TextView blueDisplay4 = (TextView) findViewById(R.id.bluevalue4);
-        blueDisplay4.setText("Blue: " + blue4);
-
-        TextView levelDisplay4 = (TextView) findViewById(R.id.level4);
-        levelDisplay4.setText("Level: Borderline");
+        TextView blueDisplay4 = (TextView) findViewById(R.id.blue4);
+        blueDisplay4.setText(blue4);
 
 
 
 
-        View colorRectangle5 = (View)findViewById(R.id.finalcolor5);
+        View colorRectangle5 = (View)findViewById(R.id.color5);
         Drawable background5 = colorRectangle5.getBackground();
         if (background5 instanceof ShapeDrawable) {
             ((ShapeDrawable)background5).getPaint().setColor(Color.rgb(Integer.parseInt(red5), Integer.parseInt(green5), Integer.parseInt(blue5)));
@@ -342,30 +280,22 @@ public class ResultsActivity extends AppCompatActivity {
         }
 
 
-        TextView colorScoreDisplay5 = (TextView) findViewById(R.id.colorScore5);
-        colorScoreDisplay5.setText(colorScore5);
 
-        TextView coordinatesDisplay5 = (TextView) findViewById(R.id.coordinates5);
-        coordinatesDisplay5.setText("(X, Y): (" + x5 +", " + y5 +")");
+        TextView redDisplay5 = (TextView) findViewById(R.id.red5);
+        redDisplay5.setText(red5);
 
-        TextView radiusDisplay5 = (TextView) findViewById(R.id.radius5);
-        radiusDisplay5.setText("Radius: " + radius5 + " pixels");
+        TextView greenDisplay5 = (TextView) findViewById(R.id.green5);
+        greenDisplay5.setText(green5);
 
-        TextView redDisplay5 = (TextView) findViewById(R.id.redvalue5);
-        redDisplay5.setText("Red: " + red5);
-
-        TextView greenDisplay5 = (TextView) findViewById(R.id.greenvalue5);
-        greenDisplay5.setText("Green: " + green5);
-
-        TextView blueDisplay5 = (TextView) findViewById(R.id.bluevalue5);
-        blueDisplay5.setText("Blue: " + blue5);
-
-        TextView levelDisplay5 = (TextView) findViewById(R.id.level5);
-        levelDisplay5.setText("Level: Borderline");
+        TextView blueDisplay5 = (TextView) findViewById(R.id.blue5);
+        blueDisplay5.setText(blue5);
 
 
 
+    }
 
-
+    public void anotherTest(View view) {
+        Intent intent = new Intent(ResultsActivity.this, ImageCapture.class);
+        startActivity(intent);
     }
 }
